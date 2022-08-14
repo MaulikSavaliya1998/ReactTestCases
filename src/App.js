cbb6274a757ee190260f55a8ea4b074e5bf1cc7f
Maulik Savaliya <savaliyamaulik98@gmail.com>
2022-08-14 16:12:04 +0530

    Initial project setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
-import CommentForm from "./components/CommentForm";
 
 function App() {
   return (
-    <div className="App" data-testid="myrootdiv">
-      <h1>Testing Basics</h1>
-      <input type="text" placeholder="type name here." />
-      <button>Test Button</button>
-      <ul>
-        <li>item 1</li>
-        <li>item 2</li>
-      </ul>
-      <CommentForm />
+    <div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
