6cbd5f069873cf47842eacbb928bd2c5107b00d7
Maulik Savaliya <savaliyamaulik98@gmail.com>
2022-08-15 15:35:49 +0530

    update App.test.js file to rollback

--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,9 @@
-import { render, screen, logRoles } from "@testing-library/react";
+import React from "react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-// ---------------Basic-------------
-test("Test 1", () => {
+test("renders learn react link", () => {
   render(<App />);
-  // logRoles(screen.getByTestId("myrootdiv"));
-  const buttonElem = screen.getByRole("button", {
-    name: "Test Button",
-    exact: false,
-  });
-  // const buttonElem = screen.queryByRole("button", {
-  //   name: "Test Button",
-  //   exact: false,
-  // });
-  // expect(buttonElem).not.toBeInTheDocument();
-  // expect(buttonElem).toBeNull();
-  expect(buttonElem).toBeInTheDocument();
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
